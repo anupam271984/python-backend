b99ade95601753a2859f37470e7f2af4afda2d90
Anupam <anupam1984upa@gmail.com>
2026-03-17 13:22:36 +0530

    added new day

--- Day2_Variables and Data Types.py
+++ Day2_Variables and Data Types.py
@@ -1,4 +1,35 @@
-# 2. Variables and Data Types
+# Variables and Data Types
+#------------------------------------------------------------------------
+# 1. Numeric data: int, float, complex
+int1 = 3
+int2 = -8
+int3 = 0
+float1 = 7.349 
+float2 =-9.0 
+float3 = 0.0000001
+complex1 = 6 + 2j
+print(int1)
+print(float1)
+print(f"The complex number is: {complex1}")
+print(f"Real part: {complex1.real}")
+print(f"Imaginary part: {complex1.imag}")
+#------------------------------------------------------------------------
+
+# List
+badges = ["Admin", "Architect", "Developer"]
+print(badges)
+
+#------------------------------------------------------------------------
+# Dictionary
+user = {"name": "Amit", "points": 50000}
+print(user)
+
+#------------------------------------------------------------------------
+#tuple = Used for data that cannot change (Immutable). It’s like a "Read-Only List
+coordinates = (12.97, 77.59)
+print(coordinates)
+
+#------------------------------------------------------------------------
 name = "Trailblazer"  # String 
 badges = 50           # Integer
 is_certified = True   # Boolean 
@@ -9,14 +40,7 @@ print(f"User {name} has {badges} badges. Certified: {is_certified}")
 # If i need to print each statement in new line then add \n
 print(f"User {name}\nhas {badges} badges.\nCertified: {is_certified}")
 
-# List
-badges = ["Admin", "Architect", "Developer"]
-print(badges)
-
-# Dictionary
-user = {"name": "Amit", "points": 50000}
-print(user)
-
-#tuple = Used for data that cannot change (Immutable). It’s like a "Read-Only List
-coordinates = (12.97, 77.59)
-print(coordinates)
+a = 1
+print(type(a))
+b = "1"
+print(type(b))
---
+++ Day3_Exercise.py
@@ -0,0 +1,11 @@
+a = 10
+b = 6
+
+print("the value of sum", a, "+", b, "is : ", a+b)
+print("the value of ", a, "-", b, "is : ", a-b)
+print("the value of ", a, "/", b, "is : ", a/b)
+print("the value of ", a, "%", b, "is : ", a%b)
+print("the value of ", a, "//", b, "is : ", a//b)
+print("the value of ", a, "*", b, "is : ", a*b)
+print("the value of ", a, "//", b, "is : ", a//b)
+print("the value of ", a, "**", b, "is : ", a**b)
